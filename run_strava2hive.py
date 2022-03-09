3a7a6575514f58bf700b6df744b368fb1f588c2a
vincesesto <vince_sesto@mail.com>
2022-03-09 16:54:54 +1300

    Update run_strava2hive.py

--- run_strava2hive.py
+++ run_strava2hive.py
@@ -2,11 +2,13 @@
 
 import os
 
-
 print("Running Strava 2 Hive")
 
-def strava_screenshot():
+def strava_screenshot(activity):
+  # Create the command to run on chrome
+  chrome_command = 'google-chrome --headless --screenshot="./screenshot"' + activity + '.png "https://www.strava.com/activities/' + activity + '"'
+  print(chrome_command)
   os.system('google-chrome --headless --screenshot="./screenshot.png" "https://www.strava.com/activities/6790387629"')
   
 print("Take screenshot of activity")  
-strava_screenshot()
+strava_screenshot(6790387629)
