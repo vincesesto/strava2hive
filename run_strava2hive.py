b299d96afa98f0a2e8f76a180d90bdf2926f42cc
vincesesto <vince_sesto@mail.com>
2022-03-09 16:40:10 +1300

    Update run_strava2hive.py

--- run_strava2hive.py
+++ run_strava2hive.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python
 
-from selenium import webdriver
-#from webdriver_manager.firefox import GeckoDriverManager
-from time import sleep
+import os
+
 
 print("Running Strava 2 Hive")
 
-def strava_screenshot(activity_url):
-  driver = webdriver.Firefox() 
-  #driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-  driver.get(activity_url)
-  sleep(1)
-  driver.get_screenshot_as_file("screenshot.png")
-  driver.quit()
-  print("end...")
+def strava_screenshot():
+  os.system('google-chrome --headless --screenshot="screenshot.png" "https://www.strava.com/activities/6790387629"')
   
 print("Take screenshot of activity")  
-strava_screenshot("https://www.strava.com/activities/6790387629")
+strava_screenshot()
