a81530d65c5635800b4bcc27cf52c220f08672bf
vincesesto <vince_sesto@mail.com>
2022-03-17 15:58:30 +1300

    Update run_strava2hive.py

--- run_strava2hive.py
+++ run_strava2hive.py
@@ -18,7 +18,7 @@ def sheets_connect(sheet_name):
   # open the google spreadsheet
   print("Test open")
   #sh = gc.open(sheet_name)
-  sh = gc.open_by_url("https://docs.google.com/spreadsheets/d/1Evd8kFy28cJlU08G2GMSOj0gA2PX2Hypte5W4s_LqtI/edit?usp=drivesdk")
+  sh = gc.open_by_url("https://docs.google.com/spreadsheets/d/1Evd8kFy28cJlU08G2GMSOj0gA2PX2Hypte5W4s_LqtI")
   #select the first sheet
   print("Test get rows")
   wks = sh[0]
