1abcbe396095694d83cfd6fd45799e64d0f64909
vincesesto <vince_sesto@mail.com>
2022-03-18 15:12:43 +1300

    Update run_strava2hive.py

--- run_strava2hive.py
+++ run_strava2hive.py
@@ -12,20 +12,31 @@ def strava_screenshot(activity):
   print(chrome_command)
   os.system(chrome_command)
   
+def get_last_activity():
+  gc = pygsheets.authorize(service_file='strava2hive.json')
+  sh = gc.open("StravaActivity)
+  #select the first sheet
+  wks = sh[0]
+  print("What is the number of rows in this sheet")
+  print(wks.row_count)
+  
 def sheets_connect(sheet_name):
   print("Test connect")
   gc = pygsheets.authorize(service_file='strava2hive.json')
   # open the google spreadsheet
   print("Test open")
   sh = gc.open(sheet_name)
-  #sh = gc.open_by_url("https://docs.google.com/spreadsheets/d/1Evd8kFy28cJlU08G2GMSOj0gA2PX2Hypte5W4s_LqtI")
   #select the first sheet
   print("Test get rows")
   wks = sh[0]
-  print(wks.get_row(2)) 
+  print(wks.get_row(2))
     
 print("Take screenshot of activity")  
 strava_screenshot(6790387629)
 
 print("Check values in hive users sheet")
 sheets_connect("HiveAthletes")
+
+get_last_activity()
+
+
