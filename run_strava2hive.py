9911ab491cc2d796d31641836caafb1634bf2a6c
vincesesto <vince_sesto@mail.com>
2022-04-06 11:44:54 +1200

    Update run_strava2hive.py

--- run_strava2hive.py
+++ run_strava2hive.py
@@ -128,6 +128,7 @@ def strava_activity(athlete_id):
   parameters = {"after": int(t.strftime("%s"))}
   response = requests.get("https://www.strava.com/api/v3/athlete/activities?per_page=5", headers=headers, params=parameters )
   activity_data = response.json()
+  print(activity_data)
   for i in range(len(activity_data)):
     activity = activity_data[i]
     if activity['type'] == "Run":
