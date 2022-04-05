538ecf993c0d30a4702adec8ef3648acf143e049
vincesesto <vince_sesto@mail.com>
2022-04-05 20:34:38 +1200

    Update run_strava2hive.py

--- run_strava2hive.py
+++ run_strava2hive.py
@@ -63,7 +63,7 @@ def activity_posted(athlete_id, activity_id):
     row = wks.get_row(i + 1)
     print("Activity ID is", activity_id)
     print(type(row[1]))
-    if str(row[1]) == activity_id:
+    if str(row[1]) == str(activity_id):
       posted = True
       print("Activity has been found, now returning True")
       break
