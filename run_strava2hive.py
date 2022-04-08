dd02106481e50993136c739eae37e455c10a47b7
vincesesto <vince_sesto@mail.com>
2022-04-08 15:13:26 +1200

    Update run_strava2hive.py

--- run_strava2hive.py
+++ run_strava2hive.py
@@ -5,6 +5,8 @@ import pygsheets
 import pandas as pd
 import requests
 import time
+from selenium import webdriver
+from time import sleep
 from datetime import datetime, timedelta
 from beem.imageuploader import ImageUploader
 from beem import Hive
@@ -13,9 +15,17 @@ from beem.nodelist import NodeList
 # Functions
 def strava_screenshot(activity):
   # Create the command to run on chrome
-  chrome_command = 'google-chrome --headless --screenshot="./screenshot_' + str(activity) + '.png" "https://www.strava.com/activities/' + str(activity) + '"'
-  print(chrome_command)
-  os.system(chrome_command)
+  #chrome_command = 'google-chrome --headless --screenshot="./screenshot_' + str(activity) + '.png" "https://www.strava.com/activities/' + str(activity) + '"'
+  #print(chrome_command)
+  #os.system(chrome_command)
+  activity_url = "https://www.strava.com/activities/" + str(activity)
+  driver = webdriver.Chrome('/bin/chromedriver')
+  driver.get(activity_url)
+  sleep(5)
+  driver.get_screenshot_as_file("screenshot.png")
+  driver.quit()
+         
+  
   
 def get_last_activity():
   # Last activity from google spreadsheet created by zapier
@@ -153,9 +163,9 @@ def post_to_hive(athlete_id, activity_details):
   print("Log - Downloading images and getting details together")
   #strava_screenshot(activity_details['id'])
   #image_path = '/home/circleci/project/screenshot_' + str(activity_details['id']) + '.png'
-  os.system('wget https://drive.google.com/open?id=16y8dMM0DupVASUj8VOq-72ZcLyFodz6q -O image.png')
-  image_path = '/home/circleci/project/image.png'
-  image_name = 'image.png'
+  os.system('wget https://drive.google.com/open?id=16y8dMM0DupVASUj8VOq-72ZcLyFodz6q -O activity_image.png')
+  image_path = '/home/circleci/project/activity_image.png'
+  image_name = 'activity_image.png'
   #image_name = 'screenshot_' + str(activity_details['id']) + '.png'
   image_uploader = ImageUploader(blockchain_instance=hive)
   img_link = image_uploader.upload(image_path, author, image_name=image_name)
@@ -207,8 +217,8 @@ def strava_activity(athlete_id):
         record_post(athlete_id, activity['id'])
         
 
-#print("Take screenshot of activity")  
-#strava_screenshot(6790387629)
+print("Take screenshot of activity")  
+strava_screenshot(6790387629)
 
 #print("Get the latest Activity")
 #activity = get_last_activity()
