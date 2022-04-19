1649f62ddbb99bdf666e61d1846b90619a8b47db
vincesesto <vince_sesto@mail.com>
2022-04-19 20:17:33 +1200

    Update run_strava2hive.py

--- run_strava2hive.py
+++ run_strava2hive.py
@@ -169,10 +169,10 @@ def post_to_hive(athlete_id, activity_details):
   duration = str(round(activity_details['duration'] / 60))
   print("Log - Downloading images and getting details together")
   strava_screenshot(activity_details['id'])
-  image_name = ''
-  img_link = {}
-  prof_image_name = ''
-  prof_img_link = {}
+  #image_name = ''
+  #img_link = {}
+  #prof_image_name = ''
+  #prof_img_link = {}
   # Get athlete profile image
   # Eventually use the image from the post as well
   if activity_details['photos']['primary'] == None:
@@ -192,12 +192,12 @@ def post_to_hive(athlete_id, activity_details):
     image_path = '/home/circleci/project/prof_image_' + str(athlete_id) + '.png'
     image_name = 'prof_image_' + str(athlete_id) + '.png'
     image_uploader = ImageUploader(blockchain_instance=hive)
-    img_link = image_uploader.upload(prof_image_path, author, image_name=prof_image_name)
+    img_link = image_uploader.upload(prof_image_path, author, image_name=image_name)
     # The screen shot is now at the bottom of the page
     prof_image_path = '/home/circleci/project/image_' + str(activity_details['id']) + '.png'
     prof_image_name = 'image_' + str(activity_details['id']) + '.png'
     prof_image_uploader = ImageUploader(blockchain_instance=hive)
-    prof_img_link = image_uploader.upload(image_path, author, image_name=image_name)
+    prof_img_link = image_uploader.upload(image_path, author, image_name=prof_image_name)
   title = activity_details['name']
   body = f'''
   ![{image_name}]({img_link['url']})
