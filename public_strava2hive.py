48603e57b39c2372e242e6c0619eb8b8333cb194
vincesesto <vince_sesto@mail.com>
2022-05-15 15:31:08 +1200

    Update public_strava2hive.py

--- public_strava2hive.py
+++ public_strava2hive.py
@@ -265,7 +265,7 @@ def strava_activity(athlete_id):
 ##################################################
 
 # Now we just have a list of Strava ID's but we will eventually make a list from our sheet
-strava_athletes = hive_work.list_athletes()
+strava_athletes = hive_work.list_athletes("Strava2HiveNewUserSignUp")
 print(strava_athletes)
 
 print("Log - Use athlete details to get activity from strava")
