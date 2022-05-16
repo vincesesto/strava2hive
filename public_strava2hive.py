6ce40ec849546c7b6df82a3348a339092ba6cb37
vincesesto <vince_sesto@mail.com>
2022-05-16 15:26:04 +1200

    Update public_strava2hive.py

--- public_strava2hive.py
+++ public_strava2hive.py
@@ -132,7 +132,7 @@ def post_to_hive(athlete_id, activity_details):
   # Get all the details including the posting keys
   athlete_details = hive_work.get_athlete(athlete_id, "Strava2HiveNewUserSignUp")
   #wif = os.getenv('POSTING_KEY')
-  wif = athlete_details[3]
+  wif = athlete_details[6]
   hive = Hive(nodes=nodes, keys=[wif])
   author = athlete_details[1]
   distance = str(round(activity_details['distance'] * .001, 2))
