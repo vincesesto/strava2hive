12e84990c895998ea5b9ddd63bd4123375c5721a
vincesesto <vince_sesto@mail.com>
2022-05-21 14:40:07 +1200

    Update pipedream_modules.py

--- pipedream_modules.py
+++ pipedream_modules.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
 
-def activity_posted_api( activity_id):
+import requests
+import json
+
+def activity_posted_api(activity_id):
+  url = 'https://eoe0c053dx0czcp.m.pipedream.net'
+  activity_query = {'activity_id': 'activity_id'}
+  header_vals = {'Content-Type': 'application/json' }
   # Use the pipedream api to test if activity has been posted
-  pass
+  try:
+    response = requests.post(url, data = activity_query, headers=header_vals)
+    return_data = response.json()
+  except:
+    print("Log - An Error occurred trying to authenticate with the {} Strava token".format(athlete[6]))
+    return False
+
+  return return_data
