2ab786ef8f6a77399908c6bc2ee6d91c4d62a7e7
vincesesto <vince_sesto@mail.com>
2022-05-21 14:15:55 +1200

    Update run_strava2hive.py

--- run_strava2hive.py
+++ run_strava2hive.py
@@ -124,6 +124,7 @@ def new_user_access_token(athlete):
                                      'code': athlete[6], 'grant_type': 'authorization_code'})
     access_info = dict()
     activity_data = response.json()
+    print(activity_data)
     access_info['access_token'] = activity_data['access_token']
     access_info['expires_at'] = activity_data['expires_at']
     access_info['refresh_token'] = activity_data['refresh_token']
