faf0e5b7771f111ee35e87d40a5ac6efc779e79d
vincesesto <vince_sesto@mail.com>
2022-05-31 14:18:21 +1200

    Update public_strava2hive.py

--- public_strava2hive.py
+++ public_strava2hive.py
@@ -238,6 +238,7 @@ def strava_activity(athlete_id):
       print("Log - Now get some more detailed information")
       detailed_activity = strava_activity_details(activity['id'], bearer_header)
       print(detailed_activity)
+      print(detailed_activity['description'])
       if detailed_activity['description'] == None:
         print("Log - Activity does not have a description, move on")
         #break
