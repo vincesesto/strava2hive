ef75e759e8b5c3c303d87c503b186886922cacec
vincesesto <vince_sesto@mail.com>
2022-06-16 09:44:27 +1200

    Update public_strava2hive.py

--- public_strava2hive.py
+++ public_strava2hive.py
@@ -185,8 +185,7 @@ def post_to_hive(athlete_id, activity_details):
   
   ![{prof_image_name}]({prof_img_link['url']})
   
-  This is an automated post by @strava2hive and is currently in BETA.
-  '''
+  ''' + post_functions.post_footer()
   parse_body = True
   self_vote = False
   #tags = ['exhaust', 'test', 'beta', 'runningproject', 'sportstalk']
