00ecf8064b3b4d62f5d0bb7c1f71b9ccaf9091c6
vincesesto <vince_sesto@mail.com>
2022-06-16 11:03:41 +1200

    Update hive_work.py

--- hive_work.py
+++ hive_work.py
@@ -101,7 +101,6 @@ def update_athlete(athlete_id, change_val, column, sheet_name):
 
 def refresh_hivesigner_token(athlete):
   # We need to update the hivesigner token every six days
-  print(athlete)
   hive_signer_info = dict()
   try:
     response = requests.post("https://hivesigner.com/api/oauth2/token?", 
@@ -109,9 +108,8 @@ def refresh_hivesigner_token(athlete):
     hive_response_data = response.json()
     hive_signer_info['hive_signer_access_token'] = hive_response_data['access_token']
     hive_signer_info['hive_signer_expires'] = int(time.time()) + 604800
-    print(athlete[10])
-    print(update_athlete(athlete[10], hive_signer_info['hive_signer_access_token'], 'G', "Strava2HiveNewUserSignUp"))
-    print(update_athlete(athlete[10], hive_signer_info['hive_signer_expires'], 'I', "Strava2HiveNewUserSignUp"))
+    update_athlete(athlete[10], hive_signer_info['hive_signer_access_token'], 'G', "Strava2HiveNewUserSignUp")
+    update_athlete(athlete[10], hive_signer_info['hive_signer_expires'], 'I', "Strava2HiveNewUserSignUp")
   except:
     print("Log - An Error occurred trying to authenticate with the {} hive token".format(athlete[5]))
     return False
