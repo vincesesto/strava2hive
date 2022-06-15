d81243d6d44c547a3f2835c2151eb76276b54d47
vincesesto <vince_sesto@mail.com>
2022-06-15 16:28:39 +1200

    Update post_functions.py

--- post_functions.py
+++ post_functions.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
-# This function has been set up to break out all the body of the post
+# This function has been set up to break out the body of the post
 
-
-
-
+def post_footer():
+  # Create a footer for our posts
+  footer = f'''
+  This is an automated post by @strava2hive and is currently in BETA.
+  '''
+  return footer
 
