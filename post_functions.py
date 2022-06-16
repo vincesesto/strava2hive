0c4bfecfade7fa846194c9154f896a5eb52c52c1
vincesesto <vince_sesto@mail.com>
2022-06-16 15:32:45 +1200

    Update post_functions.py

--- post_functions.py
+++ post_functions.py
@@ -2,6 +2,34 @@
 
 # This function has been set up to break out the body of the post
 
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import 
+
+
+# Functions
+def strava_screenshot(activity):
+  # Create the command to run on chrome
+  #chrome_command = 'google-chrome --headless --screenshot="./screenshot_' + str(activity) + '.png" "https://www.strava.com/activities/' + str(activity) + '"'
+  #print(chrome_command)
+  #os.system(chrome_command)
+  activity_url = "https://www.strava.com/activities/" + str(activity)
+  image_name = "image_" + str(activity) + ".png"
+  driver = webdriver.Chrome('/bin/chromedriver')
+  driver.get(activity_url)
+  sleep(10)
+  driver.find_element(by=By.CLASS_NAME, value="btn-accept-cookie-banner").click() 
+  driver.get_screenshot_as_file(image_name)
+  driver.quit()
+  os.system("ls -l")
+  
+def top_image(image_name, image_link):
+  # Create the top image for the post
+  top_image = f'''
+  ![{image_name}]({image_link})
+  '''
+  return top_image
+
 def post_footer():
   # Create a footer for our posts
   footer = f'''
