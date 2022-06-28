6c73ce49283d8d77e798d456e5dffeb947bb28db
vincesesto <vince_sesto@mail.com>
2022-06-29 09:40:46 +1200

    Create book_keeping.py

---
+++ book_keeping.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os
+import pygsheets
+import pandas as pd
+import requests
+import time
+import re
+import random
+import string
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+from datetime import datetime, timedelta
+from beem.imageuploader import ImageUploader
+from beem import Hive
+from beem.nodelist import 
+
+# Script to run after posting to count up records and post to accounts
+
+
+# Function to donwload activity file as csv
+
+
+
+# Function to get all users from both sheets
+
+
+
+# Maybe set up a function to count from Hive
+
+
+
+# Main workflow
