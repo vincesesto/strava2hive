60eae0069e37e6d4ec833128d848d979135a70ab
vincesesto <vince_sesto@mail.com>
2022-06-29 13:33:39 +1200

    Update book_keeping.py

--- book_keeping.py
+++ book_keeping.py
@@ -8,6 +8,7 @@ import time
 import re
 import random
 import string
+import hive_work
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
@@ -26,11 +27,12 @@ def download_sheet_as_csv(sheet_name, sheet_number):
   wks = sh[sheet_number]
   wks.export(pygsheets.ExportType.CSV)
 
-
-
-# Function to get all users from both sheets
-
-
+# Function to get all users from both dev and prod sheets
+def list_all_athletes():
+  dev_athletes = hive_work.list_athletes(6, "HiveAthletes")
+  prod_athletes = hive_work.list_athletes(10, "Strava2HiveNewUserSignUp")
+  all_athletes = dev_athletes + prod_athletes
+  return all_athletes
 
 # Maybe set up a function to count from Hive
 
@@ -45,6 +47,11 @@ print("Log - Count/Record/Comment/Upvote")
 print("Download the activity sheet to work directly with")
 download_sheet_as_csv("StravaActivity", 1)
 print(os.system("ls -l"))
+
+print("Log - get all athletes and start working through them")
+athletes = list_all_thletes()
+for i in athletes:
+  print(i)
 
 
 
