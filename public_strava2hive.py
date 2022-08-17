17ecc1d34556d39422cd4d6c3edbc733ccb2d32b
vincesesto <vince_sesto@mail.com>
2022-08-18 09:31:37 +1200

    Update public_strava2hive.py

--- public_strava2hive.py
+++ public_strava2hive.py
@@ -254,10 +254,11 @@ def strava_activity(athlete_deets):
   #response = requests.get("https://www.strava.com/api/v3/athlete/activities?per_page=3", headers=headers, params=parameters )
   response = requests.get("https://www.strava.com/api/v3/athlete/activities?per_page=3", headers=headers)
   activity_data = response.json()
-  print("what is the output to no data")
-  print(len(activity_data))
-  print(activity_data)
   for i in range(len(activity_data)):
+    if 'Authorization Error' in i:
+      print(i)
+      print("Log - there seems to be an error with loggin into strava")
+      break
     activity = activity_data[i]
     print(activity['type'])
     if activity['type'] == 'Workout':
