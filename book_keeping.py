82c2413d2f314129fb8cb05bd85a462ef826460b
vincesesto <vince_sesto@mail.com>
2022-08-26 13:32:46 +1200

    Update book_keeping.py

--- book_keeping.py
+++ book_keeping.py
@@ -197,7 +197,47 @@ if file_exists:
 # DynamoDB Stuff
 import boto3
 
+client = boto3.client(
+    'dynamodb',
+    aws_access_key_id=os.getenv('DB_ACCESS_KEY'),
+    aws_secret_access_key=os.getenv('DB_SECRET_KEY'),
+    )
 
+dynamodb = boto3.resource(
+    'dynamodb',
+    aws_access_key_id=os.getenv('DB_ACCESS_KEY'),
+    aws_secret_access_key=os.getenv('DB_SECRET_KEY'),
+    )
+
+ddb_exceptions = client.exceptions
+
+try:
+    table = client.create_table(
+        TableName='ISS_locations',
+        KeySchema=[
+            {
+                'AttributeName': 'timestamp',
+                'KeyType': 'HASH'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'timestamp',
+                'AttributeType': 'N'
+            }
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 10,
+            'WriteCapacityUnits': 10
+        }
+    }
+    print("Creating table")
+    waiter = client.get_waiter('table_exists')
+    waiter.wait(TableName='ISS_locations')
+    print("Table created")
+
+except ddb_exceptions.ResourceInUseException:
+    print("Table exists")
 
 
 
