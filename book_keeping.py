86cd0107781c6a570f4538d8c1d0ea2314de567c
vincesesto <vince_sesto@mail.com>
2022-08-26 14:16:47 +1200

    Update book_keeping.py

--- book_keeping.py
+++ book_keeping.py
@@ -199,12 +199,14 @@ import boto3
 
 client = boto3.client(
     'dynamodb',
+    region_name='ap-southeast-2',
     aws_access_key_id=os.getenv('DB_ACCESS_KEY'),
     aws_secret_access_key=os.getenv('DB_SECRET_KEY'),
     )
 
 dynamodb = boto3.resource(
     'dynamodb',
+    region_name='ap-southeast-2',
     aws_access_key_id=os.getenv('DB_ACCESS_KEY'),
     aws_secret_access_key=os.getenv('DB_SECRET_KEY'),
     )
