2452acab5b19bc41fc6effd013b72c4e7ac8b01b
vincesesto <vince_sesto@mail.com>
2022-09-09 13:39:28 +1200

    Update public_strava2hive.py

--- public_strava2hive.py
+++ public_strava2hive.py
@@ -451,6 +451,13 @@ else:
     ReturnValues="UPDATED_NEW"
   )  
 
-
-
+#Start from scratch
+#1. get a list of all the athleteId's
+#2. loop through all the athleteId's
+#	3. get the dynamo details for that athleteId
+#	4. change the last_post_date is more that 12 hours old
+#	5. check if strava token has expired, refresh if not
+#	6. check if hivesigner token has expired, refresh if not
+#	7. now see if the user has had any activities
+#
 
