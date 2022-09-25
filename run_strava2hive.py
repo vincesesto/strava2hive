101c1482d11ce1823a383fb39c9fa1eb999a1a4b
vincesesto <vince_sesto@mail.com>
2022-09-26 10:52:58 +1300

    Update run_strava2hive.py

--- run_strava2hive.py
+++ run_strava2hive.py
@@ -368,7 +368,7 @@ else:
 )
 
 print("Testing if Strava Tokens are correct")
-print(response['Items'][0]['strava_access_token'])
+print(response['Items'][0].keys())
 print(type(response['Items'][0]['strava_access_token']))
 dynamo_strava_token = response['Items'][0]['strava_access_token']
 sheet_strava_token = athlete_values[7]
