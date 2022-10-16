414fe493ba2fbc886417dc78b2d2c79364175fe3
vincesesto <vince_sesto@mail.com>
2022-10-17 11:32:10 +1300

    Update book_keeping.py

--- book_keeping.py
+++ book_keeping.py
@@ -73,7 +73,7 @@ def reblog_strava2hive(permlink):
   
 # Function to work out weekly top 10
 def create_top_10(top_10):
-  total_hbd = 10
+  total_hbd = 5
   tot_cal = 0
   for i in range(len(top_10)):
     tot_cal = tot_cal + top_10[i][1]
