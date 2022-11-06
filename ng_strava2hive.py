f6bf6bb5a92f3ff0e74ad1111df59729b118deac
vincesesto <vince_sesto@mail.com>
2022-11-06 19:03:04 +1300

    Update ng_strava2hive.py

--- ng_strava2hive.py
+++ ng_strava2hive.py
@@ -377,7 +377,7 @@ for i in athlete_list:
     print("Log - Strava Token Still Valid")
   else:
     print("Log - Strava Token Needs To Be Updated")
-    new_strava_access_token, new_strava_expires = hive_work.refresh_dynbamo_access_token(athlete_values)  
+    new_strava_access_token, new_strava_expires = hive_work.refresh_dynamo_access_token(athlete_values)  
     print("Updating strava token on dynamo")
     table = dynamodb.Table(dynamoTable)
     response = table.update_item(
