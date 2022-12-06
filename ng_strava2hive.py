e7d6482a8f829c2d58da838c930deef682090a73
vincesesto <vince_sesto@mail.com>
2022-12-07 12:32:49 +1300

    Update ng_strava2hive.py

--- ng_strava2hive.py
+++ ng_strava2hive.py
@@ -335,10 +335,10 @@ response = dynamodb.Table(dynamoTable).scan()
 
 #Start from scratch again
 #1. get a list of all the athleteId's(we are doing this the easy way for now)
-athlete_list = [101635754, 1778778, 105596627]
+athlete_list = [101635754, 1778778, 105596627, 105808129]
 #2. loop through all the athleteId's
 for i in athlete_list:
-  print(f'Log - Working throuh activity for the user {i}')
+  print(f'Log - Working throuh the next set of activity for the user {i}')
   #	3. get the dynamo details for that athleteId
   dynamodb = hive_work.dynamo_access()
   table = dynamodb.Table(dynamoTable)
