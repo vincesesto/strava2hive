c9623fbea1fd4d03e0dceed8790f4bd238a75621
vincesesto <vince_sesto@mail.com>
2023-08-07 12:12:41 +1200

    Update book_keeping.py

--- book_keeping.py
+++ book_keeping.py
@@ -180,7 +180,7 @@ new_leader_board = {}
 activity_calories = {}
 total_activity_count = 0
 
-new_week_row = 2180
+new_week_row = 2265
 
 print("Log - Tally up top athletes")
 for i in all_athletes:
@@ -223,7 +223,7 @@ print(high)
 leaders = create_leader_board(high)
 print(leaders)
 
-print(create_top_10(top10))
+#print(create_top_10(top10))
 reblog_count = 0
 print("Personal Best 80 blog posts")
 
