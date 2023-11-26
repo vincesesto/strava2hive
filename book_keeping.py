933b99a37439903e9e6d39c1030b84a39b80a529
vincesesto <vince_sesto@mail.com>
2023-11-27 10:43:35 +1300

    Update book_keeping.py

--- book_keeping.py
+++ book_keeping.py
@@ -79,7 +79,7 @@ def reblog_strava2hive(permlink, hive_token):
   
 # Function to work out weekly top 10
 def create_top_10(top_10):
-  total_hbd = 4
+  total_hbd = 5
   tot_cal = 0
   for i in range(len(top_10)):
     tot_cal = tot_cal + top_10[i][1]
@@ -225,7 +225,7 @@ print(high)
 leaders = create_leader_board(high)
 print(leaders)
 
-#print(create_top_10(top10))
+print(create_top_10(top10))
 reblog_count = 0
 print("Personal Best 80 blog posts")
 
