a3bf7138d0cba0500f5df9385c1cdecc881a187c
vincesesto <vince_sesto@mail.com>
2024-01-21 20:20:05 +1300

    Update book_keeping.py

--- book_keeping.py
+++ book_keeping.py
@@ -174,7 +174,8 @@ prod_athletes = hive_work.list_athletes(1, "Strava2HiveNewUserSignUp")
 ng_athletes = [ 'mervinthepogi', 'dennnmarc', 'valerianis', 'crysis', 'ataliba', 
                'rmsadkri', 'neuerko', 'fortune1m', 'ingi1976', 'anna-newkey', 'sabajfa', 'matthewbox', 
                'kam5iz', 'sodom-lv', 'pinkhub', 'maccazftw', 'amico.sports', 'sandralopes', 'chris-uk',
-               'itravelrox22', 'crackyup', 'buzzgoblin', 'taushifahamed', 'alfazmalek02', 'akb01']
+               'itravelrox22', 'crackyup', 'buzzgoblin', 'taushifahamed', 'alfazmalek02', 'akb01',
+               'kingtanu', 'itz.inno' ]
 all_athletes = dev_athletes + prod_athletes + ng_athletes
 
 leader_board = {}
