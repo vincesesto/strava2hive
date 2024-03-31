229625d7675ae9c9fab9c0fa15da2f6b33af0556
vincesesto <vince_sesto@mail.com>
2024-03-31 15:36:56 +1300

    Update post_functions.py

--- post_functions.py
+++ post_functions.py
@@ -43,6 +43,16 @@ def activity_summary(author, distance, activity_type, duration, calories):
   
 def post_footer():
   # Create a footer for our posts
+  footer = f'''
+  This is an automated post by @strava2hive and is currently in BETA.
+  
+  If you would like to know more about the @strava2hive service, you can checkout our [Frequently Asked Questions.](https://hive.blog/hive-176853/@strava2hive/strava2hive-frequently-asked-questions)
+  
+  '''
+  return footer
+
+def post_footer_and_image():
+  # Create a footer for our posts
   # Add in second image to post, if it is available
   footer_with_image = f'''
   This is an automated post by @strava2hive and is currently in BETA.
