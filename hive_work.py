137e25c90f9c24dadcac7cc0ada2c4c1b0ab479d
vincesesto <vince_sesto@mail.com>
2024-04-16 14:26:38 +1000

    Update hive_work.py

--- hive_work.py
+++ hive_work.py
@@ -211,9 +211,9 @@ def strava_photo_check(activity_id, strava_access_token):
   response = requests.get(strava_activity_url, headers=headers)
   photo_data = response.json()
   print(len(photo_data))
-  print(photo_data)
+  #print(photo_data)
   for i in photo_data:
-    print(i['urls']['1800'])
+    print(i['urls']['5000'])
   return photo_data
   
 def strava_activity_details(activity_id, strava_access_token):
