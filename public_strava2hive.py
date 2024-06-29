b936914ac2c101aebf756e8347aca862bc3bd0ca
vincesesto <vince_sesto@mail.com>
2024-06-29 12:50:27 +1200

    Update public_strava2hive.py

--- public_strava2hive.py
+++ public_strava2hive.py
@@ -252,8 +252,9 @@ def post_to_hive(athlete_id, activity_details, strava_access_token):
       allow_votes = True,
       extensions =  [[0,{"beneficiaries": [{"account": "strava2hive", "weight": 500}]}]])
   print("Log - Using Hivesigner to post")
-  account_deets = Account(author, blockchain_instance=hive)
-  auth = account_deets.get_blog(limit=5)
+  #account_deets = Account(author, blockchain_instance=hive)
+  #auth = account_deets.get_blog(limit=5)
+  print("Log - Testing to see if this is breaking things")
   
   broadcast_results = c.broadcast([comment.to_operation_structure(),comment_options.to_operation_structure()])
   #broadcast_results = c.broadcast([comment.to_operation_structure()])
