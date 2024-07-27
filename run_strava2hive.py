ea30ea350f613ce8a1d67f287bcc891e88a7a2f7
vincesesto <vince_sesto@mail.com>
2024-07-27 18:39:22 +1000

    Update run_strava2hive.py

--- run_strava2hive.py
+++ run_strava2hive.py
@@ -36,24 +36,6 @@ def dynamo_access():
   ddb_exceptions = client.exceptions
   return dynamodb
 
-#def strava_screenshot(activity):
-#  # Create the command to run on chrome
-#  #chrome_command = 'google-chrome --headless --screenshot="./screenshot_' + str(activity) + '.png" "https://www.strava.com/activities/' + str(activity) + '"'
-#  #print(chrome_command)
-#  #os.system(chrome_command)
-#  activity_url = "https://www.strava.com/activities/" + str(activity)
-#  image_name = "image_" + str(activity) + ".png"
-#  s = Service('/bin/chromedriver')
-#  #driver = webdriver.Chrome('/bin/chromedriver')
-#  driver = webdriver.Chrome(service=s) 
-#  driver.get(activity_url)
-#  sleep(3)
-#  #driver.find_element(by=By.CLASS_NAME, value="btn-accept-cookie-banner").click() 
-#  #driver.find_element_by_class_name("btn-accept-cookie-banner").click() 
-#  driver.get_screenshot_as_file(image_name)
-#  driver.quit()
-#  os.system("ls -l")
-
 def activity_posted(athlete_id, activity_id):
   # Check if an activity has been posted already
   gc = pygsheets.authorize(service_file='strava2hive.json')
