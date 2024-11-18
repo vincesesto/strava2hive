776bc3d6d59befd3892c718db0736154e94b1631
vincesesto <vince_sesto@mail.com>
2024-11-18 21:01:35 +1300

    Update book_keeping.py

--- book_keeping.py
+++ book_keeping.py
@@ -176,7 +176,7 @@ ng_athletes = [ 'nicklewis', 'masoom', 'budapestguide', 'bostonadventures', 'thi
                'kam5iz', 'sodom-lv', 'pinkhub', 'maccazftw', 'amico.sports', 'sandralopes', 'chris-uk',
                'itravelrox22', 'crackyup', 'buzzgoblin', 'taushifahamed', 'alfazmalek02', 'akb01',
                'kingtanu', 'itz.inno', 'disgustoid88', 'nabbas0786', 'giorgakis', 'gunting', 'bobinson',
-               'hasaanazam', 'captainman', 'kiwibloke', 'chris-uk', 'tom45p', 'vardatarmaion88']
+               'hasaanazam', 'captainman', 'kiwibloke', 'chris-uk', 'tom45p', 'vardatarmaion88', 'alessandrawhite']
 all_athletes = dev_athletes + prod_athletes + ng_athletes
 
 leader_board = {}
