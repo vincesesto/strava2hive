aae8c6a5030b6baba840b6987d0ed9257b9c674a
vincesesto <vince_sesto@mail.com>
2025-04-06 19:59:26 +1200

    Update book_keeping.py

--- book_keeping.py
+++ book_keeping.py
@@ -228,7 +228,7 @@ print(high)
 leaders = create_leader_board(high)
 print(leaders)
 
-#print(create_top_10(top10))
+print(create_top_10(top10))
 reblog_count = 0
 print("Personal Best 135 blog posts")
 
