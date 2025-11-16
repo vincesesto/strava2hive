7d707bef96c6c6b595da267e18af39e3923697dd
vincesesto <vince_sesto@mail.com>
2025-11-17 10:24:26 +1300

    Update book_keeping.py

--- book_keeping.py
+++ book_keeping.py
@@ -186,7 +186,7 @@ new_leader_board = {}
 activity_calories = {}
 total_activity_count = 0
 
-new_week_row = 1043
+new_week_row = 1166
 
 print("Log - Tally up top athletes")
 for i in all_athletes:
