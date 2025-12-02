5e405105fb770b2d48f00534b14050bad8579179
vincesesto <vince_sesto@mail.com>
2025-12-03 07:48:44 +1100

    Update run_strava2hive.py

--- run_strava2hive.py
+++ run_strava2hive.py
@@ -129,7 +129,6 @@ def strava_activity_details(activity_id, bearer_header):
   headers = {'Content-Type': 'application/json', 'Authorization': bearer_header}
   response = requests.get(strava_activity_url, headers=headers, )
   more_activity_data = response.json()
-  print(more_activity_data)
   activity_info = dict()
   try:
     activity_info['id'] = activity_id
