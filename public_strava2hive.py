9627c560dfc7ee04a62e6f5401d3fc86bec8c12d
vincesesto <vince_sesto@mail.com>
2026-01-13 10:43:02 +1100

    Update public_strava2hive.py

--- public_strava2hive.py
+++ public_strava2hive.py
@@ -238,10 +238,8 @@ def strava_activity(athlete_deets):
   print("Testing for user " + athlete_details[10])
   if athlete_details[10] == '18212962':
     response = requests.get("https://www.strava.com/api/v3/athlete/activities?per_page=5", headers=headers)
-  elif athlete_details[10] == '24345393':
-    response = requests.get("https://www.strava.com/api/v3/athlete/activities?per_page=5", headers=headers)
   elif athlete_details[10] == '185989398':
-    response = requests.get("https://www.strava.com/api/v3/athlete/activities?per_page=5", headers=headers)
+    response = requests.get("https://www.strava.com/api/v3/athlete/activities?per_page=6", headers=headers)
   else:
     response = requests.get("https://www.strava.com/api/v3/athlete/activities?per_page=2", headers=headers)
   activity_data = response.json()
