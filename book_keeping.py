925fef590183487c600c24cc8bcf4690c21d95fd
vincesesto <vince_sesto@mail.com>
2026-02-23 10:27:17 +1300

    Update new_week_row value from 2553 to 2664

--- book_keeping.py
+++ book_keeping.py
@@ -186,7 +186,7 @@ new_leader_board = {}
 activity_calories = {}
 total_activity_count = 0
 
-new_week_row = 2553
+new_week_row = 2664
 
 print("Log - Tally up top athletes")
 for i in all_athletes:
