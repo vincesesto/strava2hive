665ddfe8183f37e2082cc8fcab7020de19686738
vincesesto <vince_sesto@mail.com>
2026-08-08 06:32:33 +1200

    Update activity_id check in latlng stream function

--- image_generator.py
+++ image_generator.py
@@ -90,7 +90,7 @@ def has_gps_data(
     """
     try:
         #act_test = (activity.get("type"))
-        if activity_id == 19602551316:
+        if activity_id == 19632065485:
           return False
         else:
             lats, lons = fetch_strava_latlng_stream(access_token, activity_id, timeout_seconds=timeout_seconds)
