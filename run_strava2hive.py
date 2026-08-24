a5c1e1596438b440d85baf19b6bf363e4b309187
vincesesto <vince_sesto@mail.com>
2026-08-24 12:48:02 +1200

    Refactor update_activity call to use post_functions

--- run_strava2hive.py
+++ run_strava2hive.py
@@ -342,7 +342,7 @@ def strava_activity(athlete_deets):
           "status": "POSTED"
         }
         
-        update_activity(table, activity)
+        post_functions.update_activity(table, activity)
         
         # Work around for most recent post to be stored in HiveAthletes sheet
         hive_work.update_athlete(athlete_details[6], activity_date, "A", "HiveAthletes")
