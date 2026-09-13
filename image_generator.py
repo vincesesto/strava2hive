bd42447ea7edf5afaa88dc18488c17fcc49dd888
vincesesto <vince_sesto@mail.com>
2026-09-13 13:37:44 +1200

    Update image_generator.py

--- image_generator.py
+++ image_generator.py
@@ -90,7 +90,7 @@ def has_gps_data(
     """
     try:
         #act_test = (activity.get("type"))
-        if activity_id == 20060591841:
+        if activity_id == 20147220032:
           return False
         else:
             lats, lons = fetch_strava_latlng_stream(access_token, activity_id, timeout_seconds=timeout_seconds)
